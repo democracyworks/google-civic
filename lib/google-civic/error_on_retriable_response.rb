0076bd0157ca7a6143478bdf045bfc3b2822d38b
Troy Hill <troy@democracy.works>
2016-10-28 09:04:20 -0600

    mechanism by which retries are triggered
    for server error responses or temporary rate limit
    403 responses. throws custom exception that is
    caught by the retry middleware. only active if
    retry config is passed to client methods.

---
+++ lib/google-civic/error_on_retriable_response.rb
@@ -0,0 +1,76 @@
+module GoogleCivic
+
+  class RetriableError < Exception
+  	attr_reader :env, :response
+
+  	def initialize(env)
+  	  @env = env
+  	end
+  end
+
+  class ErrorOnRetriableResponse < Faraday::Middleware
+
+  	SERVER_ERROR_CODES = (500...600)
+
+  	TEMPORARY_REASONS = Set.new(["concurrentLimitExceeded", "rateLimitExceeded",
+  		 						 "servingLimitExceeded", "userRateLimitExceeded"])
+
+  	def initialize(app, options={})
+  	  super(app)
+  	end
+
+  	def call(env)
+  	  response = @app.call(env)
+  	  response.on_complete do |response_env|
+  	  	if retriable_error?(response)
+  	  	  raise GoogleCivic::RetriableError.new(response_env)
+  	  	end
+  	  end
+  	end
+
+  	def retriable_error?(response)
+  	  server_error?(response.status) or
+  	  temporary_rate_limit?(response)
+  	end
+
+  	def server_error?(status)
+  	  SERVER_ERROR_CODES.include? status
+  	end
+
+  	def temporary_rate_limit?(response)
+  	  if response.status == 403
+  	  	body = ::Hashie::Mash.new(::JSON.parse(response.body))
+  	  	if body.error? and body.error.errors?
+  	  	  reasons = Set.new body.error.errors.collect(&:reason)
+  	  	  TEMPORARY_REASONS.superset?(reasons)
+  	  	else
+  	  	  false
+  	  	end
+  	  else
+  	  	false
+  	  end
+  	end
+  end
+
+  class RetriesExhausted < Faraday::Middleware
+
+  	dependency do
+      require 'json' unless defined?(::JSON)
+      require 'hashie/mash'
+    end
+
+  	def initialize(app, options={})
+  	  super(app)
+  	end
+
+  	def call(env)
+  	  begin
+  	  	response = @app.call(env)
+  	  rescue GoogleCivic::RetriableError => err
+  	  	response = ::Hashie::Mash.new({status: err.env.status, headers: err.env.response_headers,
+  	  				   			       body: ::JSON.parse(err.env.body)})
+  	  end
+  	  response
+  	end
+  end
+end
